26660c245fceb0704c9c381c761d65225ab37ae6
ArvidErik <erik.arvid.hosszu@gmail.com>
2025-04-06 12:32:59 +0200

    no scraper service

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,7 @@
 package com.example.demo;
 
-import com.example.demo.model.DriverStanding;
-import com.example.demo.repository.DriverStandingRepository;
-import com.example.demo.service.ScraperService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -14,13 +9,5 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	@Bean
-	CommandLineRunner runOnStartup(ScraperService scraperService) {
-		return args -> {
-			System.out.println("🚀 Running initial scrape on startup...");
-			scraperService.scrapeDriverStandings();
-		};
 	}
 }
